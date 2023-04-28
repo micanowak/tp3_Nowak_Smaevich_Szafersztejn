a05980a123713019025b809349e65add15bf71ad
MICAELA NOWAK <47297838@ort>
2023-04-28 11:49:43 -0300

    agregamos bases que nos da la consigna

---
+++ index.js
@@ -0,0 +1,4 @@
+import { application } from "express";
+import express from "express"
+
+app.get('/pizza', async.res)
---
+++ services/pizza-services.js
@@ -0,0 +1,25 @@
+import config from './dbconfig.js';
+import sql from 'mssql';
+
+export class pizzaSerice {
+    static  getAll = async(id) => {
+
+    }
+
+    static  getById = async(id) => {
+        
+    }
+
+    static  insert = async(pizza) => {
+        
+    }
+
+    static  update = async(pizza) => {
+        
+    }
+
+    static  deleteBy = async(pizza) => {
+        
+    }
+
+}
